c00c8f4dc162a23dd31d05889160c7cd389cefe3
divanshi07 <86121922+divanshi07@users.noreply.github.com>
2022-08-20 10:58:13 +0530

    Time: 5049 ms (5.01%), Space: 13.9 MB (9.62%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,7 +1,12 @@
 class Solution:
+    dp=[]
     def fib(self, n: int) -> int:
-        if(n==0):
-            return 0
-        if(n==1):
-            return 1
-        return self.fib(n-1)+self.fib(n-2)
+        global dp 
+        dp=[-1]*31
+        dp[0]=0
+        dp[1]=1
+        if(dp[n]!=-1):
+            return dp[n]
+        ans = self.fib(n-1)+self.fib(n-2)
+        dp[n]=ans
+        return ans
